0d2119fe34a4ef0a1bbdd39120c3bf4e72edde61
Shouryan Nikam <dev.shouryan@gmail.com>
2025-10-18 18:04:15 -0400

    remove modal

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,14 +1,11 @@
 export default function BlogLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
   return (
     <>
       {children}
-      {modal}
     </>
   );
 }
